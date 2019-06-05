5a6b754184edcee3c027d98886413a9005cabb08
kevin lang <langkevina@gmail.com>
2019-06-05 23:21:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,15 @@
 class dog
   @@all = []
   attr_accessor :name
+
   def initialize(name)
     @name = name
   end
+
   def self.all?
     @@all
+  end
+
+  def self.clear_all
+    @@all = []
   end
